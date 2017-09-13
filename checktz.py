4b8ae87086424667df896e6007717e0b6587aac7
Bentechy66 <sendbenspam@yahoo.co.uk>
2017-09-13 16:33:35 +0100

    Added timezones functionality with pytz

--- checktz.py
+++ checktz.py
@@ -3,6 +3,7 @@ import asyncio
 from discord.ext import commands
 import botobject
 import time
+import getTime
 
 bot = botobject.bot
 GMT = ["BhunaBoy", "BenTechy66"]
@@ -11,7 +12,8 @@ BOT = ["Interlaced Minds Official Bot", "BenBot"]
 async def GetTime(username):
     username = username[:-5]
     if username in GMT:
-        return time.strftime("%H:%M:%S" ,time.gmtime())
+        #return time.strftime("%H:%M:%S" ,getTime.GetTime("Europe/London"))
+        return getTime.GetTime("Europe/London")
         #return "GMT"
     elif username in BOT:
         return "BOT"
---
+++ getTime.py
@@ -0,0 +1,8 @@
+import pytz
+import datetime
+
+
+def GetTime(timezone):
+    a = datetime.datetime.now() # UTC
+    b = datetime.datetime.now(tz=pytz.timezone(timezone)) # for Japan time zone
+    return b
