b7faecab264fa220c5de37f679e0981e097418ab
Bentechy66 <sendbenspam@yahoo.co.uk>
2017-09-13 19:50:22 +0100

    Added more people and fixed formatting

--- checktz.py
+++ checktz.py
@@ -6,11 +6,13 @@ import time
 import getTime
 
 bot = botobject.bot
-GMT = ["BhunaBoy", "BenTechy66", "Oliverh57", "Dylan The Spud"]
+GMT = ["BhunaBoy", "BenTechy66", "Oliverh57", "Dylan The Spud", "Geenium"]
 BOT = ["Interlaced Minds Official Bot", "BenBot"]
-GER = ["Mr. SoUndso"]
+GER = ["Mr. SoUndso", "CommandLord aka illio"]
 POL = ["Lolex"]
-
+LOS = ["BluePsychoRanger"]
+EST = ["Rai-Eclipse13", "GuineaNumber2", "GuineaPvP", "Landmine752"]
+JER = ["Simple Rick"]
 
 async def GetTime(username):
     username = username[:-5]
@@ -22,6 +24,12 @@ async def GetTime(username):
         return getTime.GetTime("Europe/Berlin")
     elif username in POL:
         return getTime.GetTime("Europe/Warsaw")
+    elif username in LOS:
+        return getTime.GetTime("America/Los_Angeles")
+    elif username in EST:
+        return getTime.GetTime("EST")
+    elif username in JER:
+        return getTime.GetTime("Asia/Jerusalem")
         
     else:
         return "??:??"
--- getTime.py
+++ getTime.py
@@ -5,4 +5,4 @@ import datetime
 def GetTime(timezone):
     a = datetime.datetime.now() # UTC
     b = datetime.datetime.now(tz=pytz.timezone(timezone)) # for Japan time zone
-    return b.strftime("%H:%M:%S") #formatting
+    return b.strftime("%H:%M") #formatting
